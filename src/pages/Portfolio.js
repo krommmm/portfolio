a65e39c8614ccf6a19f95bdc0d70f60c0b54356c
Thibault Boutaud <krom@gmx.fr>
2023-06-23 18:21:18 +0200

    ajout all

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,6 +11,26 @@ import cuisine from '../assets/projets/cuisine.png';
 import { useEffect, useState } from 'react';
 import Defilement from '../components/Defilement';
 import Carousel from '../components/carousel';
+import api from '../assets/projets/api_recherche.png';
+import calculatrice from '../assets/projets/calcu.png';
+import diapo from '../assets/projets/carrousel.png';
+import compteur from '../assets/projets/compteur.png';
+import genCitations from '../assets/projets/genCitation.png';
+import genPassword from '../assets/projets/genPassowrd.png';
+import imc from '../assets/projets/imc.png';
+import justePrix from '../assets/projets/justePrix.png';
+import memory from '../assets/projets/memoryCard.png';
+import meteo from '../assets/projets/meteo.png';
+import minuteur from '../assets/projets/minuteur.png';
+import morpion from '../assets/projets/morpion.png';
+import netflix from '../assets/projets/netflix.png';
+import pokedex from '../assets/projets/pokedex.png';
+import ppc from '../assets/projets/PPC.png';
+import saveTheme from '../assets/projets/saveTheme.png';
+import scrollInfini from '../assets/projets/scrollInfini.png';
+import todoList from '../assets/projets/todoList.png';
+import tva from '../assets/projets/tva.png';
+
 
 const Portfolio = () => {
 
@@ -24,7 +44,26 @@ const Portfolio = () => {
 		{vin: vin},
 		{vin: gw},
 		{kasa: kasa},
-		{cuisine : cuisine}
+		{cuisine : cuisine},
+		{api: api},
+		{calculatrice:calculatrice},
+		{diapo:diapo},
+		{compteur:compteur},
+		{genCitations:genCitations},
+		{genPassword:genPassword},
+		{imc:imc},
+		{justePrix:justePrix},
+		{memory:memory},
+		{meteo:meteo},
+		{minuteur:minuteur},
+		{morpion: morpion},
+		{netflix:netflix},
+		{pokedex:pokedex},
+		{ppc:ppc},
+		{saveTheme:saveTheme},
+		{scrollInfini:scrollInfini},
+		{todoList:todoList},
+		{tva:tva}
 	];
 
 	const urls = [
@@ -37,7 +76,26 @@ const Portfolio = () => {
 		{vin: "https://feteduvin.netlify.app/"},
 		{gw: "https://clonegamesworkshop.netlify.app/"},
 		{kasa: "https://kasatb.netlify.app/"},
-		{cuisine: "https://github.com/krommmm/cuisine"}
+		{cuisine: "https://cuisinefamille.net"},
+		{api:"https://unrivaled-palmier-c3461a.netlify.app/"},
+		{calculatrice:"https://zingy-alpaca-0d35ce.netlify.app/"},
+		{diapo:"https://precious-gnome-937510.netlify.app/"},
+		{compteur:"https://dynamic-churros-65a75a.netlify.app/"},
+		{genCitations:"https://monumental-daifuku-b753f0.netlify.app/"},
+		{genPassword:"https://tranquil-gelato-6d2d72.netlify.app/"},
+		{imc:"https://enchanting-narwhal-9956a2.netlify.app/"},
+		{justePrix:"https://majestic-mooncake-1126bd.netlify.app/"},
+		{memory:"https://cute-kashata-0e8671.netlify.app/"},
+		{meteo:"https://serene-jelly-c73543.netlify.app/"},
+		{minuteur: "https://dynamic-griffin-4415a4.netlify.app/"},
+		{morpion:"https://delicate-sherbet-560c87.netlify.app/"},
+		{netflix:"https://app.netlify.com/sites/idyllic-brigadeiros-32ba95"},
+		{pokedex:"https://delicate-kelpie-a58dd8.netlify.app/"},
+		{ppc:"https://stellar-hamster-fbfad2.netlify.app/"},
+		{saveTheme:"https://benevolent-cranachan-71b69d.netlify.app/"},
+		{scrollInfini:"https://helpful-frangipane-7ca1f4.netlify.app/"},
+		{todoList:"https://effortless-banoffee-37ca0b.netlify.app/"},
+		{tva:"https://thriving-souffle-7e520e.netlify.app/"}
 	];
 
 	const texte = [
@@ -50,7 +108,26 @@ const Portfolio = () => {
 		{vin: "Clone fête du vin"},
 		{vin: "Clone GamesWorkshop"},
 		{kasa: "Kasa"},
-		{cuisine: "Cuisine"}
+		{cuisine: "Cuisine"},
+		{api: "RandomUser"},
+		{calculatrice:"Calculatrice"},
+		{diapo: "Diaporama"},
+		{compteur:"Compteur de mots"},
+		{genCitations:"Générateur de citations"},
+		{genPassword: "Générateur de mdp"},
+		{imc: "Calcul d'imc"},
+		{justePrix: "Le juste prix"},
+		{memory: "Jeu de mémoire"},
+		{meteo: "Météo"},
+		{minuteur: "Minuteur"},
+		{morpion: "Jeu du morpion"},
+		{netflix:"Clone netflix"},
+		{pokedex: "Pokedex"},
+		{ppc: "Pierre papier ciseaux"},
+		{saveTheme:"Sauver un thème"},
+		{scrollInfini: "Scroll infini"},
+		{todoList: "Todo list"},
+		{tva: "Calcul de la tva"}
 	];
 
 	const [isTabletteOuMoins, setIsTabletteOuMoins] = useState(false);
