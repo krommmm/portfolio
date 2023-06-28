21faf9b52a90b8b13ae16edee5de697262b7691e
Thibault Boutaud <krom@gmx.fr>
2023-06-28 15:36:02 +0200

    correction contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ const Contact = () => {
 		<>
 			<div className="contact">
 				<div className="contact_2">
-					<h2 className="titlePage">Contat me</h2>
+					<h2 className="titlePage">Contact me</h2>
 					<div className="contact_container">
 						<i className="fa-solid fa-phone"></i>
 						<p>+33 (06) 50 47 41 78</p>
@@ -12,7 +12,9 @@ const Contact = () => {
 						<p>thibault.boutaud@gmx.fr</p>
 
 						<i className="fa-brands fa-linkedin"></i>
-						<p>https://fr.linkedin.com/in/thibault-boutaud-a72668234</p>
+						<p>
+							https://fr.linkedin.com/in/thibault-boutaud-a72668234
+						</p>
 
 						<i className="fa-brands fa-github"></i>
 						<p>https://github.com/krommmm</p>
